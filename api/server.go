d7f433068ed55ae302c9dc0eb90455df3c4511c8
EricUCL <eric.ucl@outlook.com>
2024-03-16 19:26:25 -0700

    Hashed Password

--- api/server.go
+++ api/server.go
@@ -19,8 +19,13 @@ func NewServer(store db.Store) *Server {
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("currency", validCurrency)
+		err := v.RegisterValidation("currency", validCurrency)
+		if err != nil {
+			return nil
+		}
 	}
+
+	router.POST("/users", server.createUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
